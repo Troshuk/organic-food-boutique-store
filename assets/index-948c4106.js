7674b58c08032429804c0a10d41555d8fbf61ae7
Troshuk <Troshuk@gmail.com>
2023-11-23 18:52:00 +0000

    Deploying to gh-pages from @ Troshuk/organic-food-boutique-store@b481380ae1cda5d74612f50b564c623c382f7d5b 🚀

---
+++ assets/index-948c4106.js
@@ -0,0 +1,1 @@
+import"./main-19662d93.js";const o=document.querySelector(".modal-close-btn"),e=document.querySelector(".modal-background");o.addEventListener("click",function(){e.classList.add("is-hidden")});
