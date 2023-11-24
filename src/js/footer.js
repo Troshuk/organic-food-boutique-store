f256ec32fe58d8ed9c3fa287f38625efee11b854
Kushnierova <Katerynakushnierova@gmail.com>
2023-11-24 20:30:27 +0100

    20:25

---
+++ src/js/footer.js
@@ -0,0 +1,26 @@
+// MODAL WINDOW
+const refs = {
+    openModalPolicy: document.querySelector('.js-modal-policy-open'),
+    openModalTerms: document.querySelector('.js-modal-terms-open'),
+    closeModalPolicyBtn: document.querySelector('.js-policy-close'),
+    closeModalTermsBtn: document.querySelector('.js-terms-close'),
+    policyLink: document.querySelector('.js-policy'),
+    termsLink: document.querySelector('.js-terms'),
+  };
+  
+  refs.openModalPolicy.addEventListener('click', () => {
+    onPolicyClick();
+    refs.closeModalPolicyBtn.addEventListener('click', onPolicyClick);
+  });
+  
+  refs.openModalTerms.addEventListener('click', () => {
+    onTermsClick();
+    refs.closeModalTermsBtn.addEventListener('click', onTermsClick);
+  });
+  
+  function onPolicyClick() {
+    refs.policyLink.classList.toggle('is-hidden-policy');
+  }
+  function onTermsClick() {
+    refs.termsLink.classList.toggle('is-hidden-policy');
+  }
