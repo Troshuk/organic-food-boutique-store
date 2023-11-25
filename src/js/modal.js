2649cc24a59277e21c425418e3d5afe9f1cee3d6
Andriana3 <andrianavovchok@gmail.com>
2023-11-25 02:51:08 +0100

    modal-window.js

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,7 +1,110 @@
+import FoodBotiqueApi from "./services/foodBoutiqueApi";
+import Storage from "./services/storage";
+
 const closeModalButton = document.querySelector('.modal-close-btn');
+const modalBackground = document.querySelector('.modal-background');
+const modal = document.querySelector('.modal');
+const modalImg = document.querySelector('.modal-img');
+const modalTitle = document.querySelector('.modal-title');
+const modalCategory = document.getElementById('category');
+const modalSize = document.getElementById('size');
+const modalPopularity = document.getElementById('popularity');
+const modalDetailsText = document.querySelector('.modal-details')
+const modalAboutProduct = document.querySelector('.modal-about-product');
+const modalPriceProduct = document.querySelector('.modal-price-product');
+const modalAddButton = document.querySelector('.modal-btn')
 
-const modal = document.querySelector('.modal-background');
 
-closeModalButton.addEventListener('click', function () {
-  modal.classList.add('is-hidden');
-});
+modalAddButton.addEventListener('click', changeModalBtn);
+closeModalButton.addEventListener('click', closeModalHandler);
+modalBackground.addEventListener('click', closeModalHandler);
+document.addEventListener('keydown', escapeModalHandler);
+
+
+const productById = '640c2dd963a319ea671e383b';
+
+
+async function showProductDetails(productId) {
+  try {
+    const modalProduct = await FoodBotiqueApi.getProduct(productId);
+    console.log('Product:', modalProduct)
+    modalImg.innerHTML= `<img src="${modalProduct.img}" alt="photo of product" />`;
+    modalTitle.textContent = modalProduct.name;
+    modalCategory.textContent = modalProduct.category;
+    modalSize.textContent = modalProduct.size;
+    modalPopularity.textContent = modalProduct.popularity;
+    modalAboutProduct.textContent = modalProduct.desc;
+
+    return modalProduct.data;
+
+  }catch (error) {
+    console.error('Error fetching product data:', error.message);
+  }
+}
+
+showProductDetails(productById);
+
+let modalProductInCart;
+
+modalAddButton.addEventListener('click', async () => {
+  try {
+  const cart = Storage.get(Storage.CART_KEY) || [];
+  modalProductInCart = cart.some(item => item.productId === productById);
+    if (modalProductInCart) {
+      const updatedCart = cart.filter(item => item.productId !== productById);
+      console.log('Product removed from cart:', modalProductInCart);
+      Storage.set(Storage.CART_KEY, updatedCart);
+      } else {
+     
+        const updatedCart = [...cart, { productId: productById, amount: 1 }];
+        console.log('Product added to cart:', modalProductInCart);
+        Storage.set(Storage.CART_KEY, updatedCart);
+    }
+    
+        changeModalBtn();
+}
+  catch (error) {
+    console.error('Error adding product to cart:', error.message);
+  }
+})
+ 
+
+function changeModalBtn() {
+ modalAddButton.innerHTML = `
+    <button class="modal-btn">
+      ${modalProductInCart ? 'Removed from' : 'Add to'}
+      <svg class="modal-icon-shop" width="18" height="18">
+        <use href="./img/icons.svg#icon-shopping-cart"></use>
+      </svg>
+    </button>`;
+
+  modalAddButton.classList.toggle('modal-btn-remove', modalProductInCart);
+  modalAddButton.classList.toggle('modal-btn', !modalProductInCart);
+}
+
+
+changeModalBtn();
+
+  
+  function cleanModalEventListeners() {
+    closeModalButton.removeEventListener('click', closeModalHandler);
+    modalBackground.removeEventListener('click', closeModalHandler);
+    document.removeEventListener('keydown', escapeModalHandler);
+};
+
+  modal.onclick = function (event) {
+    event.stopPropagation();
+  };
+
+  function closeModalHandler() {
+    modalBackground.classList.add('is-hidden');
+    cleanModalEventListeners();
+}
+  
+ function escapeModalHandler(event) {
+    if (event.key == 'Escape') {
+      closeModalHandler();
+   }
+}
+
+
