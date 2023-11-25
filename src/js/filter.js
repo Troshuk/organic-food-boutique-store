ea003065973b2c2c8a9113c57d1e722635bb4f56
deathfluffy <mrandreu454@gmail.com>
2023-11-25 12:42:37 +0200

    ++++

--- src/js/filter.js
+++ src/js/filter.js
@@ -1,33 +1,69 @@
-import '../css/partials/filters.css';
-import SlimSelect from 'slim-select';
 document.addEventListener('DOMContentLoaded', function () {
-  const ref = {
-    selector: document.querySelector('.categories'),
-    divSelectElement: document.querySelector('.select-element'),
-  };
-  const { selector, divSelectElement } = ref;
+  function closeOtherDropdowns(currentDropdown) {
+    const allDropdowns = document.querySelectorAll(
+      '.select-menu, .select-menu-category'
+    );
 
-  divSelectElement.classList.add('is-hidden');
-
-  fetch('https://food-boutique.b.goit.study/api/products/categories')
-    .then(response => response.json())
-    .then(data => {
-      console.log('Fetched data:', data);
-
-      if (Array.isArray(data)) {
-        const arrCategories = data.map(category => ({
-          text: category.name,
-          value: category.id,
-        }));
-
-        new SlimSelect('.categories', {
-          data: arrCategories,
-        });
-      } else {
-        console.error('Data is not an array:', data);
+    allDropdowns.forEach(dropdown => {
+      if (
+        dropdown !== currentDropdown &&
+        dropdown.classList.contains('active')
+      ) {
+        dropdown.classList.remove('active');
       }
-    })
-    .catch(error => {
-      console.error('Error fetching categories:', error);
     });
+  }
+
+  const optionMenu = document.querySelector('.select-menu');
+  const selectBtn = optionMenu.querySelector('.select-btn');
+  const options = optionMenu.querySelectorAll('.option');
+  const sBtn_text = optionMenu.querySelector('.sBtn-text');
+
+  selectBtn.addEventListener('click', function () {
+    closeOtherDropdowns(optionMenu);
+    optionMenu.classList.toggle('active');
+  });
+
+  options.forEach(option => {
+    option.addEventListener('click', function () {
+      let selectedOption = option.querySelector('.option-text').innerText;
+      sBtn_text.innerText = selectedOption;
+      optionMenu.classList.remove('active');
+    });
+  });
+
+  const optionCategory = document.querySelector('.select-menu-category');
+  const selectBtnCustom = optionCategory.querySelector('.select-btn-custom');
+  const optionsCategory = optionCategory.querySelectorAll('.option-category');
+  const sBtn_textCategory = optionCategory.querySelector('.sBtn-text-select');
+
+  selectBtnCustom.addEventListener('click', function () {
+    closeOtherDropdowns(optionCategory);
+    optionCategory.classList.toggle('active');
+  });
+
+  optionsCategory.forEach(option => {
+    option.addEventListener('click', function () {
+      let selectedOption = option.querySelector('.option-text').innerText;
+      sBtn_textCategory.innerText = selectedOption;
+      optionCategory.classList.remove('active');
+    });
+  });
+
+  // Закрытие выпадающих списков при клике вне них
+  document.addEventListener('click', function (event) {
+    const isClickInsideOptionMenu = optionMenu.contains(event.target);
+    const isClickInsideOptionCategory = optionCategory.contains(event.target);
+
+    if (!isClickInsideOptionMenu && optionMenu.classList.contains('active')) {
+      optionMenu.classList.remove('active');
+    }
+
+    if (
+      !isClickInsideOptionCategory &&
+      optionCategory.classList.contains('active')
+    ) {
+      optionCategory.classList.remove('active');
+    }
+  });
 });
