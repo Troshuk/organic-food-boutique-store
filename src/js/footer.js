d1781eaf017362a8a322f424cd71e9765fac9a03
Kushnierova <Katerynakushnierova@gmail.com>
2023-11-25 17:30:23 +0100

    17:30

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,28 +1,59 @@
 // MODAL WINDOW
 const refs = {
-    openModalPolicy: document.querySelector('.js-modal-policy-open'),
-    openModalTerms: document.querySelector('.js-modal-terms-open'),
-    closeModalPolicyBtn: document.querySelector('.js-policy-close'),
-    closeModalTermsBtn: document.querySelector('.js-terms-close'),
-    policyLink: document.querySelector('.js-policy'),
-    termsLink: document.querySelector('.js-terms'),
-  };
-  
-  refs.openModalPolicy.addEventListener('click', () => {
-    onPolicyClick();
-    refs.closeModalPolicyBtn.addEventListener('click', onPolicyClick);
-  });
-  
-  refs.openModalTerms.addEventListener('click', () => {
-    onTermsClick();
-    refs.closeModalTermsBtn.addEventListener('click', onTermsClick);
-  });
-  
-  function onPolicyClick() {
-    refs.policyLink.classList.toggle('is-hidden-policy');
-  }
-  function onTermsClick() {
-    refs.termsLink.classList.toggle('is-hidden-policy');
-  }
+  openModalPolicy: document.querySelector('.js-modal-policy-open'),
+  openModalTerms: document.querySelector('.js-modal-terms-open'),
+  closeModalPolicyBtn: document.querySelector('.js-policy-close'),
+  closeModalTermsBtn: document.querySelector('.js-terms-close'),
+  policyLink: document.querySelector('.js-policy'),
+  termsLink: document.querySelector('.js-terms'),
 
-  
+  openModalSubscribe: document.querySelector('.js-modal-subscribe-open'),
+  closeModalSubscribeBtn: document.querySelector('.js-subscribe-close'),
+  subscribeLink: document.querySelector('.js-subscribe'),
+
+  openModalNotSubscribe: document.querySelector('.js-modal-not-subscribe-open'),
+  closeModalNotSubscribeBtn: document.querySelector('.js-not-subscribe-close'),
+  notSubscribeLink: document.querySelector('.js-not-subscribe'),
+};
+
+refs.openModalPolicy.addEventListener('click', () => {
+  onPolicyClick();
+  refs.closeModalPolicyBtn.addEventListener('click', onPolicyClick);
+});
+
+refs.openModalTerms.addEventListener('click', () => {
+  onTermsClick();
+  refs.closeModalTermsBtn.addEventListener('click', onTermsClick);
+});
+
+function onPolicyClick() {
+  refs.policyLink.classList.toggle('is-hidden-policy');
+}
+function onTermsClick() {
+  refs.termsLink.classList.toggle('is-hidden-policy');
+}
+// =================MODAL WINDOW SUBSCRIBE
+import FoodBotiqueApi from './services/foodBoutiqueApi';
+
+const footerForm = document.querySelector('.footer-subscribe-form');
+
+footerForm.addEventListener('submit', onFooterFormSubmit);
+
+function onFooterFormSubmit(e) {
+  e.preventDefault();
+  const formElements = e.currentTarget.elements;
+  const email = formElements.email.value;
+
+  FoodBotiqueApi.subscribe(email)
+    .then(onSubscribeClick)
+    .catch(onNotSubscribeClick);
+}
+refs.closeModalSubscribeBtn.addEventListener('click', onSubscribeClick);
+function onSubscribeClick() {
+  refs.subscribeLink.classList.toggle('is-hidden-subscribe');
+}
+
+refs.closeModalNotSubscribeBtn.addEventListener('click', onNotSubscribeClick);
+function onNotSubscribeClick() {
+  refs.notSubscribeLink.classList.toggle('is-hidden-not-subscribe');
+}
