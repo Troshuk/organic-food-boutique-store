06258309ff6bdc2c1cf9950691f2bfa64b1b6ab6
KaterynaMal <96122506+KaterynaMal@users.noreply.github.com>
2023-11-27 14:39:05 +0200

    render +

--- src/js/popular-products.js
+++ src/js/popular-products.js
@@ -1,61 +1,89 @@
-import Cart from "./services/Cart";
+import Cart from './services/Cart';
+// import FoodBotiqueApi from './FoodBoutiqueApi';
+import Filter from './services/Filter';
+// import updateCartItemCount from './header';
+// import openModalProductDetails from './modal';
 
+const popularProductContainer = document.querySelector(
+  '.popular-products-container'
+);
+Filter.getPopularProducts().then(products => {
+  const productElements = products.map(createProductItem);
+  const listElement = document.createElement('ul');
+  listElement.className = 'popular-products-list';
 
-// const product = {
-//     _id: '',
-// }
-// Cart.add(product);
+  productElements.map(cartElement => {
+    const productId = cartElement.dataset.productId;
+    const isProductInCart = !!Cart.getProduct(productId);
 
+    cartElement.querySelector('.basket-button').style.display = isProductInCart
+      ? 'none'
+      : 'block';
+    cartElement.querySelector('.basket-button-icon-check').style.display =
+      isProductInCart ? 'block' : 'none';
 
-//   static async getPopularProducts(limit = 5) {
-//     const response = await api.get(POPULAR_PRODUCTS_ENDPOINT, {
-//       params: { limit },
-//     });
+    //   Or Icon change!
 
-//     return response.data;
-//   }
-const popularProductsList = document.querySelector('.popular-products-list');
-const popularProduct = document.querySelector('.popular-products-item');
-const popularProductBtn = document.querySelector('.basket-button');
+    // cartElement.querySelector('.popular-products-cart-icon').style.display =
+    //   isProductInCart ? 'none' : 'block';
+    // cartElement.querySelector('.popular-products-icon-check').style.display =
+    //     isProductInCart ? 'block' : 'none';
 
+    listElement.appendChild(cartElement);
+  });
 
-// popularProduct.getProduct(id);
-// popularProduct.addEventListener('click', (getPopularProducts)); 
-// popularProductBtn.addEventListener('click', (e));
+  popularProductContainer.appendChild(listElement);
 
+  listElement.addEventListener('click', ({ target }) => {
+    const cartElement = target.closest('LI');
+    const cartButton = target.closest('BUTTON');
 
+    if (cartElement?.nodeName !== 'LI') {
+      return;
+    }
 
-// const galleryList = document.querySelector(".gallery");
+    const productId = cartElement.dataset.productId;
 
-function createPopularProductList() {
-  return popularProduct
-      .map(({
-          _id,
-name,
-img,
-category,
-// price,
-size,
-// is10PercentOff,
-popularity }) => {
-      return `<li class="popular-products-item">
-  <div class="popular-products-img-container">
-        <img
-          class="${product.img}"
-          src="https://ftp.goit.study/img/so-yummy/ingredients/640c2dd963a319ea671e3676.png"
-          alt=""
-          width="56"
-          height="56"
-        />
-        <svg class="popular-products-discount-icon" width="20" height="20">
-          <use href="./img/icons.svg#icon-discount"></use>
-        </svg>
-      </div>
+    if (cartButton?.nodeName !== 'BUTTON') {
+      openModalProductDetails(productId, reRenderCartIcon);
+      return;
+    }
 
-      <div class="popular-products-description-thumb">
-        <h3 class="popular-products-name${product.name}">Askee</h3>
+    if (cartButton?.nodeName === 'BUTTON') {
+      const isProductInCart = !!Cart.getProduct(productId);
 
-        <button class="basket-button" type="button">
+      if (isProductInCart) {
+        Cart.delete(productId);
+      } else {
+        Cart.add(products.find(e => e._id === productId));
+      }
+
+      updateCartItemCount();
+
+      cartElement.querySelector('.basket-button').style.display =
+        isProductInCart ? 'block' : 'none';
+      cartElement.querySelector('.basket-button-icon-check').style.display =
+        isProductInCart ? 'none' : 'block';
+    }
+  });
+});
+
+function createProductItem({ _id, img, name, category, size, popularity }) {
+  const productItem = document.createElement('li');
+  productItem.className = 'popular-products-item';
+  productItem.dataset.productId = _id;
+
+  productItem.innerHTML = `
+    <div class="popular-products-img-container">
+      <img class="popular-products-img" src="${img}" alt="${name}" width="56" height="56"/>
+      <svg class="popular-products-discount-icon" width="20" height="20">
+        <use href="./img/icons.svg#icon-discount"></use>
+      </svg>
+    </div>
+
+    <div class="popular-products-description-thumb">
+      <h3 class="popular-products-name">${name}</h3>
+          <button class="basket-button" type="button">
           <svg class="popular-products-cart-icon" width="12" height="12">
             <use href="./img/icons.svg#icon-shopping-cart"></use>
           </svg>
@@ -64,133 +92,139 @@ popularity }) => {
           <svg class="popular-products-icon-check" width="12" height="12">
             <use href="./img/icons.svg#icon-check"></use>
           </svg>
-        </button>
-        <div class="popular-products-description-container">
-          <p class="popular-products-description">
-            Category:
-            <span class="${product.category} popular-description">Fresh Produce</span>
-          </p>
+        </button> 
 
-          <p class="popular-products-description">
-            Size:
-            <span class="${product.size} popular-description">16 oz</span>
-          </p>
+        
 
-          <p class="popular-products-description">
-            Popularity:
-            <span class="${product.popularity} popular-description">3</span>
-          </p>
-        </div>
+      <div class="popular-products-description-container">
+        <p class="popular-products-description">
+          Category:
+          <span class="category popular-description">${category}</span>
+        </p>
+
+        <p class="popular-products-description">
+          Size:
+          <span class="size popular-description">${size}</span>
+        </p>
+
+        <p class="popular-products-description">
+          Popularity:
+          <span class="popularity popular-description">${popularity}</span>
+        </p>
       </div>
-</li>`;
-    })
-    .join("");
+    </div>
+  `;
+
+  return productItem;
 }
 
-popularProductsList.insertAdjacentHTML("beforeend", createPopularProductList());
+//---------------------------------------------------------------
 
+//     document.addEventListener('DOMContentLoaded', async () => {
+//   try {
+//     const popularProducts = await FoodBotiqueApi.getPopularProducts();
 
+//     const popularProductsList = document.querySelector('.popular-products-list');
 
-// popularProductBusketButton.addEventListener('click', (e)) = {
-//     if (e.popularProductBusketButton.onclick) {
-//         popular-products-cart-icon = popular-products-icon-check
-//    }
-// }
+//     popularProducts.forEach((product) => {
+//       const productItem = createProductItem(product);
+//       popularProductsList.appendChild(productItem);
+//     });
 
-//  static add(product, quantity = 1) {
-//     Cart.update(product, quantity);
-//   }
+//       const addToCartButtons = document.querySelectorAll('.basket-button');
+//     addToCartButtons.forEach((button) => {
+//       button.addEventListener('click', (event) => {
+//         const productId = event.currentTarget.dataset.productId;
+//         addToCart(productId);
 
-// Cart.add(popularProduct);
+//         // Змінюємо клас кнопки на 'basket-button-icon-check'
+//   // button.classList.remove('basket-button');
+//         // button.classList.add('basket-button-icon-check');
+//         //   або змінюємо іконку та стиль кнопки
+//           svg.classList.remove('popular-products-cart-icon');
+//          svg.classList.add('popular-products-icon-check')
 
-// const popularProductsEndpoint = 'https://food-boutique.b.goit.study/api/v1/popular-products';
-
-// export default class PopularProducts {
-//   static async fetch() {
-//     try {
-//       const response = await axios.get(popularProductsEndpoint);
-//       const products = response.data;
-
-//       // Render each product card
-//       products.forEach((product) => {
-//         const card = PopularProducts.createProductCard(product);
-//         // Append the card to the popular products section
-//         const popularProductsSection = document.getElementById('popular-products');
-//         popularProductsSection.querySelector('.popular-products-list').appendChild(card);
 //       });
-//     } catch (error) {
-//       console.error('Error fetching popular products:', error);
+//     });
+//       const productItems = document.querySelectorAll('.popular-products-item');
+//     productItems.forEach((item) => {
+//       item.addEventListener('click', (event) => {
+//         // Викликаємо функцію, яка відобразить модальне вікно
+//         //   тестовий варіант!! до стягнення оновлення
+
+//           const productId = cartElement.dataset.productId;
+
+//     if (cartButton?.nodeName !== 'BUTTON') {
+//       openModalProductDetails(productId, reRenderCartIcon);
+//       return;
 //     }
+//       });
+//     });
+
+//   } catch (error) {
+//     console.error('Error fetching popular products:', error);
 //   }
+//     });
 
-//   static createProductCard(product) {
-//     // Create product card elements
-//     const card = document.createElement('li');
-//     card.className = 'popular-products-item';
+// //     function displayModal(productItem) {
 
-//     card.innerHTML = `
-//       <div class="popular-products-img-container">
-//         <img class="popular-products-img" src="${product.image}" alt="${product.name}" width="56" height="56"/>
-//         <svg class="popular-products-discount-icon" width="20" height="20">
-//           <use href="./img/icons.svg#icon-discount"></use>
+// //
+// //     }
+
+// function createProductItem(product) {
+//   const productItem = document.createElement('li');
+//   productItem.className = 'popular-products-item';
+
+//   productItem.innerHTML = `
+//     <div class="popular-products-img-container">
+//       <img class="popular-products-img" src="${product.img}" alt="${product.name}" width="56" height="56"/>
+//       <svg class="popular-products-discount-icon" width="20" height="20">
+//         <use href="./img/icons.svg#icon-discount"></use>
+//       </svg>
+//     </div>
+
+//     <div class="popular-products-description-thumb">
+//       <h3 class="popular-products-name">${product.name}</h3>
+
+//       <button class="basket-button" type="button" onclick="addToCart('${product.id}')">
+//         <svg class="popular-products-cart-icon" width="12" height="12">
+//           <use href="./img/icons.svg#icon-shopping-cart"></use>
 //         </svg>
-//       </div>
-
-//       <div class="popular-products-description-thumb">
-//         <h3 class="popular-products-name">${product.name}</h3>
-
-//         <button class="basket-button" type="button" onclick="PopularProducts.addToCart('${product.id}')">
-//           <svg class="popular-products-cart-icon" width="12" height="12">
-//             <use href="./img/icons.svg#icon-shopping-cart"></use>
+//       </button>
+//         <button class="basket-button-icon-check">
+//           <svg class="popular-products-icon-check" width="12" height="12">
+//             <use href="./img/icons.svg#icon-check"></use>
 //           </svg>
 //         </button>
-        
-//         <div class="popular-products-description-container">
-//           <p class="popular-products-description">
-//             Category:
-//             <span class="category popular-description">${product.category}</span>
-//           </p>
 
-//           <p class="popular-products-description">
-//             Size:
-//             <span class="size popular-description">${product.size}</span>
-//           </p>
+//
 
-//           <p class="popular-products-description">
-//             Popularity:
-//             <span class="popularity popular-description">${product.popularity}</span>
-//           </p>
-//         </div>
+//       <div class="popular-products-description-container">
+//         <p class="popular-products-description">
+//           Category:
+//           <span class="category popular-description">${product.category}</span>
+//         </p>
+
+//         <p class="popular-products-description">
+//           Size:
+//           <span class="size popular-description">${product.size}</span>
+//         </p>
+
+//         <p class="popular-products-description">
+//           Popularity:
+//           <span class="popularity popular-description">${product.popularity}</span>
+//         </p>
 //       </div>
-//     `;
+//     </div>
+//   `;
 
-//     return card;
-//   }
-
-//   static addToCart(productId) {
-//     const product = '640c2dd963a319ea671e3676'/* Fetch detailed product information using productId */;
-//     const quantity = 1; // You can change this as needed
-
-//     // Check if the product is already in the cart
-//     const existingProduct = Cart.getProduct(product);
-//     if (existingProduct) {
-//       // Product is already in the cart, update the icon to a check mark
-//       // Optionally, you can display a message to the user
-//       // or handle it in your preferred way
-//       // Update the button icon or display a message
-//       alert('Product is already in the cart!');
-//     } else {
-//       // Product is not in the cart, add it
-//       Cart.add(product, quantity);
-//       // Optionally, you can display a success message to the user
-//       // or handle it in your preferred way
-//       // Update the button icon or display a success message
-//       alert('Product added to the cart!');
-//     }
-//   }
+//   return productItem;
 // }
 
-// // Fetch popular products when the page loads
-// document.addEventListener('DOMContentLoaded', () => {
-//   PopularProducts.fetch();
-// });
+//  async function addToCart(productId) {
+
+//      const product =  await FoodBotiqueApi.getProduct(productId);
+//     Cart.add(product);
+// }
+
+// ----------------------------------------------------------------
