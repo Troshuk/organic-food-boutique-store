74ecc81d67707bf711aefdd362fd93128bdd338f
KaterynaMal <96122506+KaterynaMal@users.noreply.github.com>
2023-11-28 20:25:19 +0200

    scroll correction

--- src/js/cart.js
+++ src/js/cart.js
@@ -6,11 +6,30 @@ import './modal-success';
 updateCartItemCount();
 
 // Scroll (::after) always on visible
+// document.addEventListener('DOMContentLoaded', function() {
+//   const basketListWrapper = document.querySelector('.basket-list-wrapper');
+
+//   basketListWrapper.addEventListener('scroll', function() {
+//     const scrollTop = basketListWrapper.scrollTop;
+//       basketListWrapper.style.setProperty('--scroll-top', scrollTop + 'px');
+    
+//   });
+// });
+
+
 document.addEventListener('DOMContentLoaded', function() {
   const basketListWrapper = document.querySelector('.basket-list-wrapper');
+  const basketList = document.querySelector('.cart-basket-list');
+
+  const containerHeight = basketListWrapper.clientHeight;
+  const listHeight = basketList.scrollHeight;
+  const scrollbarHeight = (containerHeight / listHeight) * containerHeight;
+
+  basketListWrapper.style.height = scrollbarHeight + 'px';
 
   basketListWrapper.addEventListener('scroll', function() {
     const scrollTop = basketListWrapper.scrollTop;
+    basketListWrapper.style.height = scrollbarHeight +35 + 'px'
       basketListWrapper.style.setProperty('--scroll-top', scrollTop + 'px');
     
   });
