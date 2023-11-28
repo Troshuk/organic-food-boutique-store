f20110002329e63432aa2ebf0618856619c3f133
KaterynaMal <96122506+KaterynaMal@users.noreply.github.com>
2023-11-28 14:40:49 +0200

    always visible scroll

--- src/js/cart.js
+++ src/js/cart.js
@@ -4,3 +4,14 @@ import './modal-success';
 
 // Update cart count on initial load of the page
 updateCartItemCount();
+
+// Scroll (::after) always on visible
+document.addEventListener('DOMContentLoaded', function() {
+  const basketListWrapper = document.querySelector('.basket-list-wrapper');
+
+  basketListWrapper.addEventListener('scroll', function() {
+    const scrollTop = basketListWrapper.scrollTop;
+      basketListWrapper.style.setProperty('--scroll-top', scrollTop + 'px');
+    
+  });
+});
