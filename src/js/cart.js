f4cb820c15e27ee5f4357b6428ac4e9382e4a3ec
Kushnierova <Katerynakushnierova@gmail.com>
2023-11-29 23:57:36 +0100

    23:57

--- src/js/cart.js
+++ src/js/cart.js
@@ -4,3 +4,21 @@ import './modal-success';
 
 // Update cart count on initial load of the page
 updateCartItemCount();
+
+let countValue = 1;
+
+const spanQuantity = document.querySelector('.quantity');
+
+const DecrBtn = document.querySelector('button[data-action="decrement"]');
+
+DecrBtn.addEventListener('click', () => {
+  countValue -= 1;
+  spanQuantity.textContent = countValue;
+});
+
+const IncrBtn = document.querySelector('button[data-action="increment"]');
+
+IncrBtn.addEventListener('click', () => {
+  countValue += 1;
+  spanQuantity.textContent = countValue;
+});
