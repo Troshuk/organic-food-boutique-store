37d72959c4e56d61316e50814f846a1c2bae7e1b
deathfluffy <mrandreu454@gmail.com>
2023-11-29 16:16:29 +0200

    ++++

--- src/js/filter.js
+++ src/js/filter.js
@@ -1,3 +1,7 @@
+import FoodBotiqueApi from './services/FoodBoutiqueApi';
+import Filter from './services/Filter';
+import fetchProducts from './product-list';
+
 const optionMenu = document.querySelector('.select-menu');
 const selectBtn = optionMenu.querySelector('.select-btn');
 const options = optionMenu.querySelectorAll('.options');
@@ -8,6 +12,8 @@ const selectBtnCustom = optionCategory.querySelector('.select-btn-custom');
 const optionsCategory = optionCategory.querySelector('.options');
 const sBtn_textCategory = optionCategory.querySelector('.sBtn-text-select');
 
+const searchIcon = document.getElementById('search-icon');
+const searchInput = document.getElementById('search-item');
 function closeDropDown() {
   closeOtherDropdowns(null);
 }
@@ -52,8 +58,76 @@ selectBtnCustom.addEventListener('click', function (event) {
 optionsCategory.addEventListener('click', function (event) {
   const clickedOption = event.target.closest('.option-category');
   if (clickedOption) {
-    let selectedOption = clickedOption.querySelector('.option-text').innerText;
-    sBtn_textCategory.innerText = selectedOption;
+    let selectedCategory = clickedOption.dataset.originalCategory;
+    sBtn_textCategory.innerText = selectedCategory.replace(/_/g, ' ');
     optionCategory.classList.remove('active');
+
+    Filter.setCategory(selectedCategory);
+
+    fetchProducts();
   }
 });
+
+const ul = optionsCategory;
+ul.innerHTML = '';
+
+const originalCategories = await Filter.getCategories();
+
+originalCategories.forEach(category => {
+  const li = document.createElement('li');
+  li.className = 'option-category';
+  li.dataset.originalCategory = category;
+
+  const span = document.createElement('span');
+  span.className = 'option-text';
+  span.textContent = category.replace(/_/g, ' ');
+
+  li.appendChild(span);
+  ul.appendChild(li);
+});
+
+const showAllOption = createShowAllOption();
+ul.appendChild(showAllOption);
+
+function createOption(optionName) {
+  const li = document.createElement('li');
+  li.className = 'option-category';
+  li.dataset.category = optionName;
+
+  const span = document.createElement('span');
+  span.className = 'option-text';
+  span.textContent = optionName;
+
+  li.appendChild(span);
+  return li;
+}
+
+function createShowAllOption() {
+  const showAllOption = document.createElement('li');
+  showAllOption.className = 'option-category';
+  showAllOption.dataset.category = 'Show All';
+
+  const span = document.createElement('span');
+  span.className = 'option-text';
+  span.textContent = 'Show All';
+
+  showAllOption.appendChild(span);
+  return showAllOption;
+}
+
+searchIcon.addEventListener('click', function () {
+  const keyword = searchInput.value.trim();
+
+  if (keyword) {
+    Filter.setKeyword(keyword);
+
+    fetchProducts();
+  }
+});
+function updateContentBasedOnCategory(category) {
+  const savedOptions = storage.getFromStorage('options');
+  savedOptions.category = category ? category : null;
+  localStorage.setItem('options', JSON.stringify(savedOptions));
+
+  fetchProducts();
+}
